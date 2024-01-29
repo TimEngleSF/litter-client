fed4d8310a2db3abbb65ed08f627702dc0f638ff
Tim Engle <timothy.englew@gmail.com>
2024-01-29 11:23:44 -0800

    Rename folders

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'vite', 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'vite', 'dist', 'index.html'));
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}`);
 });
